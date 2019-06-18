940015ad6107905ad716935ac09523991c800853
liammoroney <liam.moroney14@gmail.com>
2019-06-18 15:18:36 +0100

    Added styles to posts. Moved each post to a card with content, listing the title, number of upvotes and more info. Added thumbnails when not provided.

--- src/Components/Posts.tsx
+++ src/Components/Posts.tsx
@@ -1,20 +1,95 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import grey from "@material-ui/core/colors/grey";
+import { Card, CardContent, Typography, CardMedia } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  post: {
+    border: `1px solid ${theme.palette.divider}`,
+    margin: theme.spacing(0.5, 0),
+    backgroundColor: grey[200],
+    display: "flex",
+    "&:hover": {
+      borderColor: "black"
+    }
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  content: {
+    flex: "1 0 auto"
+  },
+  thumbnail: {
+    width: 100,
+    minWidth: 100,
+    height: 100,
+    backgroundColor: "gray",
+    color: "white"
+  },
+  votes: {
+    display: "flex",
+    flexDirection: "column",
+    margin: 5,
+    textAlign: "center",
+    justifyContent: "center",
+    minWidth: 60
+  }
+}));
 
 export interface PostType {
+  author: string;
+  subreddit: string;
   title: string;
+  thumbnail: string;
+  ups: number;
+  downs: number;
 }
-export type PostsType = [PostType];
+export type PostsType = PostType[];
 
 interface PostsProps {
   posts: PostsType;
 }
 
-const Posts = (props: PostsProps) => (
-  <ul>
-    {props.posts.map((post, i) => (
-      <li key={i}>{post.title}</li>
-    ))}
-  </ul>
-);
+const renderThumbnail = (classes: any, thumbnail: string) => {
+  switch (thumbnail) {
+    case "spoiler":
+    case "nsfw":
+    case "default":
+      return (
+        <div className={classes.thumbnail}>
+          <Typography variant="h5">{thumbnail}</Typography>
+        </div>
+      );
+    default:
+      return <CardMedia className={classes.thumbnail} image={thumbnail} />;
+  }
+};
+
+const Posts = (props: PostsProps) => {
+  const classes = useStyles();
+  return (
+    <ul>
+      {props.posts.map((post, i) => (
+        <Card className={classes.post} key={i}>
+          <div className={classes.votes}>
+            <Typography>{post.ups}</Typography>
+          </div>
+          {renderThumbnail(classes, post.thumbnail)}
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+                {post.title}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                u/{post.author} - r/{post.subreddit}
+              </Typography>
+            </CardContent>
+          </div>
+        </Card>
+      ))}
+    </ul>
+  );
+};
 
 export default Posts;
