10a27b23ad6089d604e8dee0e7cd830c78136310
liammoroney <liam.moroney14@gmail.com>
2019-06-20 20:23:30 +0100

    Added styles and prop/state checking

--- src/Containers/AsyncApp.tsx
+++ src/Containers/AsyncApp.tsx
@@ -1,39 +1,68 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component, MouseEvent } from "react";
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
 import {
   selectSubreddit,
   fetchPostsIfNeeded,
   invalidateSubreddit
 } from "../actions";
 import Picker from "../Components/Picker";
-import Posts from "../Components/Posts";
+import Posts, { PostsType } from "../Components/Posts";
+import { withStyles } from "@material-ui/core/styles";
 
-class AsyncApp extends Component {
-  constructor(props) {
+const styles = (theme: any) => ({
+  root: {
+    padding: theme.spacing(0, 2)
+  }
+});
+
+export interface AsyncAppProps {
+  dispatch: Dispatch<any>;
+  selectedSubreddit: string;
+  isFetching: boolean;
+  lastUpdated: number;
+  posts: PostsType;
+  classes: any;
+}
+
+interface PostsBySubreddit {
+  [index: string]: {
+    isFetching: boolean;
+    lastUpdated: number;
+    items: PostsType;
+  };
+}
+
+export interface AsyncAppState {
+  selectedSubreddit: string;
+  postsBySubreddit: PostsBySubreddit;
+}
+
+class AsyncApp extends Component<AsyncAppProps, AsyncAppState> {
+  public constructor(props: AsyncAppProps) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleRefreshClick = this.handleRefreshClick.bind(this);
   }
 
-  componentDidMount() {
+  public componentDidMount() {
     const { dispatch, selectedSubreddit } = this.props;
     dispatch(fetchPostsIfNeeded(selectedSubreddit));
   }
 
-  componentDidUpdate(prevProps) {
+  public componentDidUpdate(prevProps: AsyncAppProps) {
     if (this.props.selectedSubreddit !== prevProps.selectedSubreddit) {
       const { dispatch, selectedSubreddit } = this.props;
       dispatch(fetchPostsIfNeeded(selectedSubreddit));
     }
   }
 
-  handleChange(nextSubreddit) {
+  private handleChange(nextSubreddit: string) {
     this.props.dispatch(selectSubreddit(nextSubreddit));
     this.props.dispatch(fetchPostsIfNeeded(nextSubreddit));
   }
 
-  handleRefreshClick(e) {
+  private handleRefreshClick(e: MouseEvent) {
     e.preventDefault();
 
     const { dispatch, selectedSubreddit } = this.props;
@@ -41,14 +70,20 @@ class AsyncApp extends Component {
     dispatch(fetchPostsIfNeeded(selectedSubreddit));
   }
 
-  render() {
-    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props;
+  public render() {
+    const {
+      selectedSubreddit,
+      posts,
+      isFetching,
+      lastUpdated,
+      classes
+    } = this.props;
     return (
-      <div>
+      <div className={classes.root}>
         <Picker
           value={selectedSubreddit}
           onChange={this.handleChange}
-          options={["reactjs", "frontend"]}
+          options={["all", "all/top", "all/hot"]}
         />
         <p>
           {lastUpdated && (
@@ -72,21 +107,14 @@ class AsyncApp extends Component {
   }
 }
 
-AsyncApp.propTypes = {
-  selectedSubreddit: PropTypes.string.isRequired,
-  posts: PropTypes.array.isRequired,
-  isFetching: PropTypes.bool.isRequired,
-  lastUpdated: PropTypes.number,
-  dispatch: PropTypes.func.isRequired
-};
-
-function mapStateToProps(state) {
+function mapStateToProps(state: AsyncAppState) {
   const { selectedSubreddit, postsBySubreddit } = state;
   const { isFetching, lastUpdated, items: posts } = postsBySubreddit[
     selectedSubreddit
   ] || {
     isFetching: true,
-    items: []
+    items: [],
+    lastUpdated: Date.now()
   };
 
   return {
@@ -97,4 +125,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(AsyncApp);
+export default withStyles(styles)(connect(mapStateToProps)(AsyncApp));
